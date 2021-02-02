170d0fde0c8256c620158ce799b2968b5416fe20
Piotr Rybka <piotr.rybka82@gmail.com>
2021-02-02 21:56:04 +0100

    initial commit

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+    bool armWaving = false;
+
+    public string ArmWavingPropertyName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // wyjsciowe ustawienie
+        transform.GetComponent<Animator>().SetBool(ArmWavingPropertyName, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // jesli nacisnieto spacje
+        if (Input.GetKeyDown(KeyCode.Space))
+            armWaving = !armWaving;
+        
+        if (armWaving) 
+            transform.GetComponent<Animator>().SetBool(ArmWavingPropertyName, true);
+        else
+            transform.GetComponent<Animator>().SetBool(ArmWavingPropertyName, false);
+    }
+}
