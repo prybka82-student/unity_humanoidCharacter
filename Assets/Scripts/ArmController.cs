08f8edddbc50279379950c65c91b3530e0bf3ea0
Piotr Rybka <piotr.rybka82@gmail.com>
2021-02-03 19:59:06 +0100

    wersja 2

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -6,25 +6,27 @@ public class ArmController : MonoBehaviour
 {
     bool armWaving = false;
 
-    public string ArmWavingPropertyName;
-
     // Start is called before the first frame update
     void Start()
     {
-        // wyjsciowe ustawienie
-        transform.GetComponent<Animator>().SetBool(ArmWavingPropertyName, false);
+        transform.GetComponent<Animator>().SetBool("reka", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // jesli nacisnieto spacje
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             armWaving = !armWaving;
+        }
         
         if (armWaving) 
-            transform.GetComponent<Animator>().SetBool(ArmWavingPropertyName, true);
+        {
+            transform.GetComponent<Animator>().SetBool("reka", true);
+        }
         else
-            transform.GetComponent<Animator>().SetBool(ArmWavingPropertyName, false);
+        {
+            transform.GetComponent<Animator>().SetBool("reka", false);
+        }
     }
 }
